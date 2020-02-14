8bbe90c29b2978b45d994a74849f1c046a41dfca
Raul Souza Lima <raulsouzalima@gmail.com>
2020-02-14 16:00:28 -0300

    Remove useless test

--- spec/active_merchant_adyen_extension_spec.rb
+++ spec/active_merchant_adyen_extension_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe ActiveMerchantAdyenExtension do
   it "has a version number" do
     expect(ActiveMerchantAdyenExtension::VERSION).not_to be nil
   end
-
-  xit "does something useful" do
-    expect(false).to eq(true)
-  end
 end
